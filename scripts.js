948f76837fcf4d8f5460da06386e571aaf0ff916
CamDavi5 <camrdavis.biz@gmail.com>
2022-02-27 18:35:23 -0600

    first commit

---
+++ scripts.js
@@ -0,0 +1,49 @@
+/*name = "Cameron";
+var name;
+console.log(name);*/
+
+/*setName();
+function setName() {
+    var name = "Cameron"
+    console.log(name);
+}
+
+console.log("This is only the beginning!");
+let value = calcAverage(2, 2);
+console.log("BEHOLD, AVERAGE:", value);
+function calcAverage(num1, num2) {
+    console.log("We be calculating...");
+    let average = (num1+num2)/2;
+    return average;
+}*/
+
+/*let fruits = ["apple", "orange", "grape"];
+//let favFruit;
+
+function firstFruit() {
+    console.log(fruits[0]);
+    let favFruit = fruits[0];
+    
+    function printFav() {
+        console.log(favFruit);
+        let leastFav = fruits[1];   
+    }
+    console.log(leastFav);
+    printFav();
+}
+
+
+
+firstFruit();
+//printFav();*/
+
+whateverYouWouldLike();
+function whateverYouWouldLike() {
+    console.group('Hello Cameron');
+}
+
+
+let func = function logText() {
+    alert("Feel the healing beat!");
+}
+func();
